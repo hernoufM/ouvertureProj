2e7e15e9c9ef11e8c45b990dbcaae50b70a6d960
Hernouf Mohamed <hernouf@yandex.ru>
2019-11-20 20:55:55 +0100

    aa

--- test.ml
+++ test.ml
@@ -9,9 +9,9 @@ let random_number_in_list l =
 let l_without_r l r =
   let rec aux l r res=
     match l with
-      |[]-> res
-      |h::t -> if (r==1) then (aux t (r-1) res)
-          else (aux t (r-1) (res@[h]))
+    |[]-> res
+    |h::t -> if (r==1) then (aux t (r-1) res)
+      else (aux t (r-1) (res@[h]))
   in aux l r [];;
 
 (* return p with r on the head   *)
@@ -40,8 +40,8 @@ let extraction_alea l p =
 let generate_list_1_to_n n =
   let rec aux n res =
     match n with
-      | 1 -> 1::res
-      | n -> aux (n-1) (n::res)
+    | 1 -> 1::res
+    | n -> aux (n-1) (n::res)
   in aux n [];;
 
 (* this function completely empties the list l and populates the list p by *)
@@ -49,9 +49,9 @@ let generate_list_1_to_n n =
 let empty_list_with_alea l p f =
   let rec aux l p =
     match l with
-      |[]-> p
-      |_ -> let c = f l p
-          in aux (fst c) ( snd c )
+    |[]-> p
+    |_ -> let c = f l p
+      in aux (fst c) ( snd c )
   in aux l p;;
 
 
@@ -61,29 +61,29 @@ let gen_permutation n =
   in
   let p = []
   in
-    empty_list_with_alea l p extraction_alea;;
+  empty_list_with_alea l p extraction_alea;;
 
 (* ctrl y  et ctrl f*)
 (************************ Question 1.3 ************************)
 type 'a binary_tree =
-    | Empty
-    | Node of 'a * 'a binary_tree * 'a binary_tree;;
+  | Empty
+  | Node of 'a * 'a binary_tree * 'a binary_tree;;
 (*  left son        right son*)
 
 let insert_a tree a =
   let rec aux tree =
     match tree with
-      |Empty -> Node (a,Empty,Empty)
-      |Node(r,left,right) ->
-          if (a<r) then Node (r,aux left, right)
-          else Node (r,left, aux right)
+    |Empty -> Node (a,Empty,Empty)
+    |Node(r,left,right) ->
+      if (a<r) then Node (r,aux left, right)
+      else Node (r,left, aux right)
   in aux tree ;;
 
 let liste_to_arbre l =
   let rec aux l tree=
     match l with
-      | [] -> tree
-      | x::q -> aux q (insert_a tree x)
+    | [] -> tree
+    | x::q -> aux q (insert_a tree x)
   in aux l Empty;;
 
 (************************ Question 2.4 ************************)
@@ -91,28 +91,53 @@ let liste_to_arbre l =
 let convert_tree_to_string tree =
   let rec aux tree res =
     match tree with
-      |Empty -> res
-      |Node(r,left,right) ->
-          "("^(aux left "")^")"^(aux right "")
+    |Empty -> res
+    |Node(r,left,right) ->
+      "("^(aux left "")^")"^(aux right "")
   in aux tree "";;
 
 let l=liste_to_arbre (gen_permutation 9);;
 convert_tree_to_string (l);;
 
 let liste_parantheses tree =
-  let rec aux tree res =
+  let res1 = ref [] and res2 = ref [] in
+  let rec aux tree res1 res2 =
     match tree with
-    | Empty -> []
-    | Node(val,ag,ad) ->  let rec appartient tree liste =
-                            match liste with
-                            | [] -> false
-                            | (x,stringX)::q -> if stringX=(convert_tree_to_string tree)
-                              then true
-                              else appartient tree q
-      in if (appartient tree !res) then aux ag res;
+    | Empty -> ([],[])
+    | Node(v,ag,ad) ->  let rec appartient tree liste =
+                          match liste with
+                          | [] -> false
+                          | (x,stringX)::q -> if stringX=(convert_tree_to_string tree)
+                            then true
+                            else appartient tree q
+      in (if not (appartient tree !res1) then res1:= (List.append !res1 [(v, (convert_tree_to_string tree))])
+      else
+        let rec racine_eq tree liste =
+             match liste with
+             | [] -> -1
+             | (x,stringX)::q -> if stringX=(convert_tree_to_string tree)
+               then x
+               else racine_eq tree q
+         in res2:= (List.append !res2 [(v,racine_eq tree !res1)]));
+      (if not (ag = Empty) then let (a,b) = aux ag res1 res2 in res1:=a; res2:=b);
+      (if not (ad = Empty) then let (a,b) = aux ad res1 res2 in res1:=a; res2:=b);
+      (!res1,!res2);
+        in
+        aux tree res1 res2;;
+
+let v = [4;2;1;3;8;6;5;7;9] in
+liste_parantheses (liste_to_arbre v);;
 
 type valeurABRC = (int* int list) list;;
 
-type ABRC =
+type abrc =
   | EmptyABRC
-  | NodeABRC of valeurABRC * (ABRC ref * int) * (ABRC ref * int);;
+  | NodeABRC of valeurABRC * (abrc ref * int) * (abrc ref * int);;
+
+let rec insert tree a =
+  match tree with
+  | EmptyABRC  -> NodeABRC (a,((ref EmptyABRC),0),((ref EmptyABRC),0))
+  | NodeABRC(v,(refL,etL),(refR,etR))  ->
+    if (a<v)
+    then NodeABRC (v,(ref (insert !refL a),etL),(refR,etR))
+    else NodeABRC (v,(refL,etL),(ref (insert !refR a) ,etR));;
